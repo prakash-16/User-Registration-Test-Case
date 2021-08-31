97c45261104380aabf68ba47fcddd24f019fd944
Prakash Goud <prakash.goud@morningstar.com>
2021-08-31 22:26:48 +0530

    [Prakash] - Added Junit parameterised test to validate multiple entry of Email-Id

--- src/test/java/com/bridgelabz/User/Registration/Test/UserRegistrationRunner.java
+++ src/test/java/com/bridgelabz/User/Registration/Test/UserRegistrationRunner.java
@@ -6,44 +6,27 @@ import com.bridgelabz.User.Registration.Main.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.junit.runner.RunWith;
 
 @RunWith(Parameterized.class)
 public class UserRegistrationRunner {
 
-	private String firstName;
-	private String lastName;
-	private String mobileNumber;
 	private String email;
-	private String password;
-	private boolean value;
 
-	public UserRegistrationRunner(String firstName, String lastName, String mobileNumber, String email,
-			String password, boolean value) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.mobileNumber = mobileNumber;
+	public UserRegistrationRunner(String email) {
 		this.email = email;
-		this.password = password;
-		this.value = value;
 	}
 
 	@Parameterized.Parameters
 	public static Collection UserRegistration() {
-		Object[][] input = { { "Karan", "Mehta", "91 9876543210", "abc.xyz@bl.co.in", "Qwerty16@", true},
-				{ "Navin", "Sharma", "91 8876543210", "abc@gmail.com", "Qwerty18*", true} };
-		
+		Object[][] input = { { "abc.xyz@bl.co.in" }, { "abc@gmail.com" }, { "abc111@abc.com" },
+				{ "abc.100@abc.co.in" } };
+
 		return Arrays.asList(input);
 	}
-	
-	@Test
-	public void testUserData() {
-		assertEquals(value,UserRegistration.validateName(firstName));
-		assertEquals(value,UserRegistration.validateName(lastName));
-		assertEquals(value,UserRegistration.validateMobileNumber(mobileNumber));
-		assertEquals(value,UserRegistration.validateEmail(email));
-		assertEquals(value,UserRegistration.validatePassword(password));
-	}
 
+	@Test
+	public void testEmailId() {
+		assertEquals(true, UserRegistration.validateEmail(email));
+	}
 }
