1aafddeca0fdb2e4523201511a0ecac9010a9b4c
Prakash Goud <prakash.goud@morningstar.com>
2021-08-31 19:38:51 +0530

    [Prakash] - Added the condition of Rule - 3 to validate user's password

--- src/test/java/com/bridgelabz/User/Registration/Test/UserRegistrationRunner.java
+++ src/test/java/com/bridgelabz/User/Registration/Test/UserRegistrationRunner.java
@@ -26,7 +26,7 @@ public class UserRegistrationRunner {
 	}
 	@Test
 	public void checkPasswordIsValidOrNot() {
-		assertEquals(true, UserRegistration.validatePassword("Abcdefghi"));
+		assertEquals(true, UserRegistration.validatePassword("Qwerty16"));
 	}
 	
 }
