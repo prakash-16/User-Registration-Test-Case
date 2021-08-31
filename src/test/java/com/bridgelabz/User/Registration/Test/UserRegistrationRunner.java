b7129b75f3e2d5f162f8bf5d5775078cd28a2053
Prakash Goud <prakash.goud@morningstar.com>
2021-08-31 22:17:40 +0530

    [Prakash] - Added Junit test to validate user data

--- src/test/java/com/bridgelabz/User/Registration/Test/UserRegistrationRunner.java
+++ src/test/java/com/bridgelabz/User/Registration/Test/UserRegistrationRunner.java
@@ -4,18 +4,46 @@ import java.util.*;
 import static org.junit.Assert.*;
 import com.bridgelabz.User.Registration.Main.*;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
 
+@RunWith(Parameterized.class)
 public class UserRegistrationRunner {
 
-	@Test
-	public void checkEmailIdIsValidOrNot() {
-		assertEquals(true, UserRegistration.validateEmail("abc@yahoo.com"));
-		assertEquals(true, UserRegistration.validateEmail("abc-100@abc.net"));
-		assertEquals(true, UserRegistration.validateEmail("abc111@abc.com"));
-		assertEquals(true, UserRegistration.validateEmail("abc-100@abc.net.au"));
-		assertEquals(true, UserRegistration.validateEmail("abc.100@abc.co.in"));
-		assertEquals(true, UserRegistration.validateEmail("abc.xyz@bridgelabz.co.in"));
+	private String firstName;
+	private String lastName;
+	private String mobileNumber;
+	private String email;
+	private String password;
+	private boolean value;
 
+	public UserRegistrationRunner(String firstName, String lastName, String mobileNumber, String email,
+			String password, boolean value) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.password = password;
+		this.value = value;
+	}
+
+	@Parameterized.Parameters
+	public static Collection UserRegistration() {
+		Object[][] input = { { "Karan", "Mehta", "91 9876543210", "abc.xyz@bl.co.in", "Qwerty16@", true},
+				{ "Navin", "Sharma", "91 8876543210", "abc@gmail.com", "Qwerty18*", true} };
+		
+		return Arrays.asList(input);
+	}
+	
+	@Test
+	public void testUserData() {
+		assertEquals(value,UserRegistration.validateName(firstName));
+		assertEquals(value,UserRegistration.validateName(lastName));
+		assertEquals(value,UserRegistration.validateMobileNumber(mobileNumber));
+		assertEquals(value,UserRegistration.validateEmail(email));
+		assertEquals(value,UserRegistration.validatePassword(password));
 	}
 
 }
